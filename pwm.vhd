576fdeef536a297762aaf8b05bb46e467e5b5957
Fabiola de las Casas Escardó <65742805+facasas17@users.noreply.github.com>
2021-04-15 22:04:24 -0300

    Modifico generics y port

--- pwm.vhd
+++ pwm.vhd
@@ -1,27 +1,23 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
---use ieee.math_real.all;
---use IEEE.std_logic_arith.all;
 
 entity pwm is
 	generic(
-			divider_cnt: integer := 25000; -- Definicion de frecuencia
-			pwm_resolution: integer := 8   -- Resolucion máxima del pwm
+			pwm_resolution: integer := 8  -- Resolucion máxima del pwm
 	);
 	port(
 		clk_i: in std_logic;
 		rst_i: in std_logic;
 		duty_cycle_i: in unsigned(pwm_resolution-1 downto 0);
-		cnt_o: out unsigned(pwm_resolution-1 downto 0);
-		clk_o: out std_logic;
+		divider_cnt_i: in unsigned(15 downto 0);
 		pwm_o: out std_logic
 	);
 end;
 
 architecture pwm_arq of pwm is
 
-	signal clk_cnt: integer := 0;
+	signal clk_cnt: unsigned(15 downto 0) := to_unsigned(250,16);
 	signal aux_clk_o: std_logic := '0';
 	signal aux_cnt_o: unsigned(pwm_resolution-1 downto 0) := to_unsigned(0,pwm_resolution);
 	
@@ -30,16 +26,15 @@ begin
 	CLK_DIVIDER: process(clk_i,rst_i)
 	begin
 		if (rst_i = '1') then
-			clk_cnt <= 1;
+			clk_cnt <= to_unsigned(1,16);
 			aux_clk_o <= '0';
 		elsif rising_edge(clk_i) then
-			clk_cnt <= clk_cnt + 1;
-			if clk_cnt = divider_cnt then
+			clk_cnt <= clk_cnt + to_unsigned(1,16);
+			if clk_cnt = divider_cnt_i then
 				aux_clk_o <= not aux_clk_o;
-				clk_cnt <= 0;
+				clk_cnt <= to_unsigned(0,16);
 			end if;
 		end if;
-		clk_o <= aux_clk_o;
 	end process;
 
 	CONTADOR: process(aux_clk_o,rst_i)
@@ -49,12 +44,9 @@ begin
 		elsif rising_edge(aux_clk_o) then
 			aux_cnt_o <= aux_cnt_o + to_unsigned(1,pwm_resolution);
 		end if;
-		cnt_o <= aux_cnt_o;
 	end process;
 	
-	pwm_o <= '1' when (to_integer(aux_cnt_o) < to_integer(duty_cycle_i))
+	pwm_o <= '1' when (to_integer(aux_cnt_o) <= to_integer(duty_cycle_i))
 	else '0';
-	
-
 	
 end;
--- pwm_tb.vhd
+++ pwm_tb.vhd
@@ -11,48 +11,43 @@ architecture pwm_tb_arq of pwm_tb is
 	-- Declaracion de componente
 	component pwm is
 		generic(
-				divider_cnt: integer := 25000; -- Definicion de frecuencia
 				pwm_resolution: integer := 8   -- Resolucion máxima del pwm
 		);
 		port(
 			clk_i: in std_logic;
 			rst_i: in std_logic;
 			duty_cycle_i: in unsigned(pwm_resolution-1 downto 0);
-			cnt_o: out unsigned(pwm_resolution-1 downto 0);
-			clk_o: out std_logic;
+			divider_cnt_i: in unsigned(15 downto 0);
 			pwm_o: out std_logic
 		);
 	end component;
 	
-	constant divider_cnt_tb: integer := 25000;
-	constant pwm_resolution_tb: integer := 4;
+	constant duty_cycle_aux: integer := 122;
+	constant pwm_resolution_tb: integer := 8;
 
 	-- Señales de prueba
 	signal clk_i_tb: std_logic := '0';
 	signal rst_i_tb: std_logic := '0';
-	signal duty_cycle_i_tb: unsigned(pwm_resolution_tb-1 downto 0) := to_unsigned(0,pwm_resolution_tb);
-	signal cnt_o_tb: unsigned(pwm_resolution_tb-1 downto 0);
-	signal clk_o_tb: std_logic;
+	signal duty_cycle_i_tb: unsigned(pwm_resolution_tb-1 downto 0) := to_unsigned(122,pwm_resolution_tb);
+	signal divider_cnt_i_tb: unsigned(15 downto 0) := to_unsigned(2500,16);
 	signal pwm_o_tb: std_logic;
 	
 begin
 	-- Señales de excitacion
 	clk_i_tb <= not clk_i_tb after 10 ns;		
 	--rst_i_tb <= '0' after 10 ns;
-	duty_cycle_i_tb <= 	to_unsigned(8,pwm_resolution_tb);
+	duty_cycle_i_tb <= 	to_unsigned(duty_cycle_aux,pwm_resolution_tb);
 	
 	-- Instanciacion del componente a probar
 	DUT: pwm
 		generic map (
-			divider_cnt => divider_cnt_tb,
 			pwm_resolution => pwm_resolution_tb
 		)
 		port map(
 			clk_i => clk_i_tb,
 			rst_i => rst_i_tb,
 			duty_cycle_i => duty_cycle_i_tb,
-			cnt_o => cnt_o_tb,
-			clk_o => clk_o_tb,
+			divider_cnt_i => divider_cnt_i_tb,
 			pwm_o => pwm_o_tb
 		);
 	
